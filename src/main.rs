755e805787234bb432fd752351b8866d3a40a2ac
Massimo2710 <massimo.dantonio@gmail.com>
2022-04-01 19:39:59 +0200

    add boolean def

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    //unsigned integer
+    let unsigned: u8 = 100;
+
+    //signed integer
+    let signed: i8 = -15;
+
+    //float
+    let float: f32 =1.2;
+
+    println!("unsign: {} sign: {} float {}", unsigned,signed, float);    
+
+  //  println!("Hello, world!,{} {}",a,b);
+
+  // char- can only be
+  let letter ="c1232";
+  let emoji = "\u{1F600}"; // :D
+
+  println!("letter: {}, emoji: {}", letter , emoji);
+
+  //booléen déclaration
+  let is_true: bool =true;
+
+  println!("isTrue: {}", is_true);
+}
